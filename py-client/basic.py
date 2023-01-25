a9ac4a3225450030e75c9e935e4366c90f23c773
indieka900 <indiekaj@gmail.com>
2023-01-25 10:48:53 +0300

    api

--- py-client/basic.py
+++ py-client/basic.py
@@ -1,5 +1,6 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 result = requests.get(endpoint,json={"firstName":"Joseph"})
-print(result.json())
+print(result.text)
